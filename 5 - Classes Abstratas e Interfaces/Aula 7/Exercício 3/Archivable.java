7aba75488b9f6103ab2485f969b61c25e5cad05a
Pedro Augusto Serafim Belo <79765850+PedroASB@users.noreply.github.com>
2022-02-17 11:04:32 -0300

    Delete 5 - Classes Abstratas e Interfaces/Aula 7/Exercício 3 directory

--- 5 - Classes Abstratas e Interfaces/Aula 7/Exercício 3/Archivable.java
+++
@@ -1,6 +0,0 @@
-package Exercício3;
-import java.util.UUID;
-
-public interface Archivable extends Cloneable {
-	UUID getID(); // public and abstract
-}
--- 5 - Classes Abstratas e Interfaces/Aula 7/Exercício 3/Cloneable.java
+++
@@ -1,5 +0,0 @@
-package Exercício3;
-
-public interface Cloneable {
-	Object getClone(); // public and abstract
-}
