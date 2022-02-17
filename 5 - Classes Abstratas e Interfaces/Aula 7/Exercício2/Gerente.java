1f752a0b77c8ff0f861c4b16a48ab907e8dce1a2
Pedro Augusto Serafim Belo <79765850+PedroASB@users.noreply.github.com>
2022-02-17 10:45:06 -0300

    Update Gerente.java

--- 5 - Classes Abstratas e Interfaces/Aula 7/Exercício2/Gerente.java
+++ 5 - Classes Abstratas e Interfaces/Aula 7/Exercício2/Gerente.java
@@ -6,13 +6,13 @@ public class Gerente extends Funcionario implements Archivable {
 	private UUID id;
 	private double bonusMensal;
 
-	public Gerente(){
+	public Gerente() {
 		this.id = UUID.randomUUID();
 	}
 
 	public Gerente(String nome, String cpf, String telefone,
 				   Endereco endereco, String matricula,
-				   double salario, double bonusMensal){
+				   double salario, double bonusMensal) {
 		super(nome, cpf, telefone, endereco, matricula, salario);
 		this.id = UUID.randomUUID();
 		this.bonusMensal = bonusMensal;
@@ -21,6 +21,7 @@ public class Gerente extends Funcionario implements Archivable {
 	@Override
 	public String toString() {
 		return "Nome: " + nome +
+				"\nID: " + id +
 				"\nCPF: " + cpf +
 				"\nTelefone: " + telefone +
 				"\n| Endereco |\n" + endereco +
@@ -44,6 +45,7 @@ public class Gerente extends Funcionario implements Archivable {
 		Gerente cloneGerente = new Gerente(nome, cpf, telefone,
 				cloneEndereco, matricula, salario, bonusMensal);
 
+		// o "clone" gera um ID novo, mas ele deve ser o mesmo do gerente
 		cloneGerente.id = this.id;
 
 		return cloneGerente;
