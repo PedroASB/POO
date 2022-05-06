21a4fed0b602580ba2aa04d2c4aef7914495afae
Pedro Augusto Serafim Belo <79765850+PedroASB@users.noreply.github.com>
2022-05-06 13:51:45 -0300

    Update Application.java

--- Listas de Exercícios/Lista 2/Exercício 1/Cenário 1/Application.java
+++ Listas de Exercícios/Lista 2/Exercício 1/Cenário 1/Application.java
@@ -1,34 +1,25 @@
 public class Application {
 	public static void main(String args[]){
-		Musica musica1 = new Musica("Nome1", "Tipo1", 1995);
-		Musica musica2 = new Musica("Nome2", "Tipo2", 2005);
-		Musica musica3 = new Musica("Nome3", "Tipo3", 2015);
+		Musica musica1 = new Musica("Meu Brasil", "MPB", 1993);
+		Musica musica2 = new Musica("The Unbelievable Song", "Pop", 2015);
+		Musica musica3 = new Musica("Criatividade Ltda.", "Rock", 2008);
 
-		musica1.addCompositor(new Compositor("Comp1_1", "Brasileiro"));
-		musica1.addCompositor(new Compositor("Comp1_2", "Brasileiro"));
+		musica1.addCompositor(new Compositor("Carlos", "Brasileiro"));
+		musica1.addCompositor(new Compositor("Larissa", "Brasileira"));
 
-		musica2.addCompositor(new Compositor("Comp2_1", "Estadunidense"));
-		musica2.addCompositor(new Compositor("Comp2_2", "Canadense"));
-		musica2.addCompositor(new Compositor("Comp2_3", "Estadunidense"));
+		musica2.addCompositor(new Compositor("John", "Estadunidense"));
+		musica2.addCompositor(new Compositor("Michael ", "Canadense"));
+		musica2.addCompositor(new Compositor("Mary", "Estadunidense"));
 
-		musica3.addCompositor(new Compositor("Comp3_1", "Brasileiro"));
+		musica3.addCompositor(new Compositor("Mateus", "Brasileiro"));
 
 		System.out.println("-- Musica 1 --");
-		System.out.println(musica1);
-		for (Compositor compositor : musica1.getCompositores()){
-			System.out.println(compositor);
-		}
+		System.out.print(musica1);
 
 		System.out.println("\n-- Musica 2 --");
-		System.out.println(musica2);
-		for (Compositor compositor : musica2.getCompositores()){
-			System.out.println(compositor);
-		}
+		System.out.print(musica2);
 
 		System.out.println("\n-- Musica 3 --");
-		System.out.println(musica3);
-		for (Compositor compositor : musica3.getCompositores()){
-			System.out.println(compositor);
-		}
+		System.out.print(musica3);
 	}
 }
